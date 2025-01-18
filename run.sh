e1a4cb376a71e5510f9ddaccd9acced683b95b91
Wolfdv1 <dan@danw.pl>
2025-01-18 03:09:34 +0100

    init

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -z "$BOT_TOKEN" ]
+then
+echo "Discord integration bot token not set, token will be left blank"
+fi
+if [ -z "$BOT_CHANNEL" ] 
+then
+echo "Discord integration channel ID not set, channel will be left blank"
+fi
+cat <(head -n4 ./server-overrides/config/Discord-Integration_empty.toml) <(echo -e '\tbotToken = "'$BOT_TOKEN'"') <(echo -e '\tbotChannel = "'$BOT_CHANNEL'"') <(tail -n +5 ./server-overrides/config/Discord-Integration_empty.toml) > ./server-overrides/config/Discord-Integration.toml
+rm ./server-overrides/config/Discord-Integration_empty.toml
+zip -r -0 ./data/wip.mrpack ./server-overrides ./modrinth.index.json
+docker compose up -d
+docker attach wip-mc-1
