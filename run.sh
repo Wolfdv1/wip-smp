490172883319e8534436516df7e5cf1048632baa
Wolfdv1 <dan@danw.pl>
2025-01-18 14:06:06 +0100

    fix VoiceChat Port ptotocol and setup script

--- run.sh
+++ run.sh
@@ -8,7 +8,6 @@ then
 echo "Discord integration channel ID not set, channel will be left as-is"
 fi
 cat <(head -n4 ./server-overrides/config/Discord-Integration_empty.toml) <(echo -e '\tbotToken = "'$BOT_TOKEN'"') <(echo -e '\tbotChannel = "'$BOT_CHANNEL'"') <(tail -n +5 ./server-overrides/config/Discord-Integration_empty.toml) > ./server-overrides/config/Discord-Integration.toml
-rm ./server-overrides/config/Discord-Integration_empty.toml
 zip -r -0 ./data/wip.mrpack ./server-overrides ./modrinth.index.json
 docker compose up -d
 docker attach wip-smp-mc-1
